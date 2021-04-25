8b45751855eeacce2eb4d16fa07ed51f8b389ee4
zaqk <zachking1218@gmail.com>
2021-04-25 18:49:39 +0300

    update /api/login to /api/auth/login

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ class App extends Component {
       window.web3 = new Web3(window.ethereum);
       await window.ethereum.enable();
 
-      const loginUrl = 'http://localhost:8000/api/login'
-      const protectedUrl = 'http://localhost:8000/api/secret';
+      const loginUrl = 'http://localhost:8000/api/auth/login'
+      const protectedUrl = 'http://localhost:8000/api/auth/secret';
 
       const chainId = window.web3.currentProvider.networkVersion;
       const ethAddress = window.web3.utils.toChecksumAddress(
